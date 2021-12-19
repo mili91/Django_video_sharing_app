80bac15cb4e82d0ded957c68e9c97c90200fcf08
Muhammad Akbari <86768786+mili91@users.noreply.github.com>
2021-12-19 15:40:27 +0330

    feat (video_view): make index page show videos saved in the database
    - add paginator to the index view and template
    
    - add a name to the index page

--- video/views.py
+++ video/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from .models import Video
-
+from django.core.paginator import Paginator
 
 def index(request):
-    return render(request, 'video/index.html')
+    video_list = Video.objects.all()
+    
+    videos = Paginator(video_list, 3)
+    
+    grouped_videos = []
+    for page in videos.page_range:
+        page_objects = videos.page(page).object_list
+        grouped_videos.append(page_objects)
+
+    context = {'grouped_videos' : grouped_videos}
+    return render(request, 'video/index.html', context)
 
 def video(request, video_id):
     the_video = Video.objects.get(pk=video_id)
